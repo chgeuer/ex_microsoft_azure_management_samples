e3b2741a36c89d201b422b43b4812224a552b82f
Christian Geuer-Pollmann <chgeuer@microsoft.com>
2018-05-03 13:33:30 +0200

    Move code around

--- lib/ex_microsoft_azure_management_samples.ex
+++ lib/ex_microsoft_azure_management_samples.ex
@@ -1,19 +1,15 @@
 defmodule ExMicrosoftAzureManagementSamples do
+  alias Microsoft.Azure.Management.Connection, as: AzureManagementClient
+  alias Microsoft.Azure.Management.Resources.Api.ResourceGroups
+  alias Microsoft.Azure.Management.Compute.Api.VirtualMachineSizes
+
   @moduledoc """
-  Documentation for ExMicrosoftAzureManagementSamples.
+  Showcases the Azure management API.
   """
 
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> ExMicrosoftAzureManagementSamples.hello
-      :world
-
-  """
-  def hello do
-    :world
+  def connection() do
+    token()
+    |> AzureManagementClient.new()
   end
 
   def subscription_id() do
@@ -21,7 +17,7 @@ defmodule ExMicrosoftAzureManagementSamples do
   end
 
   def tenant_id() do
-     Application.get_env(:ex_microsoft_azure_management_samples, :tenant_id)
+    Application.get_env(:ex_microsoft_azure_management_samples, :tenant_id)
   end
 
   def token() do
@@ -30,12 +26,44 @@ defmodule ExMicrosoftAzureManagementSamples do
       |> Path.absname()
       |> Path.join(".azure")
       |> Path.join("accessTokens.json")
-      |> IO.inspect()
       |> File.read!()
       |> Poison.decode!()
-      |> Enum.filter(&( &1["_authority"] == "https://login.microsoftonline.com/" <>  tenant_id()))
+      |> Enum.filter(&(&1["_authority"] == "https://login.microsoftonline.com/" <> tenant_id()))
       |> List.last()
 
     token
+  end
+
+  def list_resource_groups() do
+    conn = connection()
+
+    api_version = "2018-02-01"
+
+    {:ok, %{value: groups}} =
+      conn
+      |> ResourceGroups.resource_groups_list(
+        api_version,
+        subscription_id()
+      )
+
+    groups
+    |> Enum.map(&(&1 |> Map.get(:name)))
+  end
+
+  def list_vm_sizes() do
+    conn = connection()
+    api_version = "2017-12-01"
+    location = "westeurope"
+
+    {:ok, %{value: sizes}} =
+      conn
+      |> VirtualMachineSizes.virtual_machine_sizes_list(
+        location,
+        api_version,
+        subscription_id()
+      )
+
+    sizes
+    |> Enum.map(&(&1 |> Map.get(:name)))
   end
 end
--- test/ex_microsoft_azure_management_samples_test.exs
+++ test/ex_microsoft_azure_management_samples_test.exs
@@ -1,69 +1,15 @@
 defmodule ExMicrosoftAzureManagementSamplesTest do
   use ExUnit.Case
 
-  alias Microsoft.Azure.Management.Connection, as: AzureManagementClient
-  alias Microsoft.Azure.Management.Resources.Api.ResourceGroups
-  alias Microsoft.Azure.Management.Compute.Api.VirtualMachineSizes
-
-  def subscription_id() do
-    Application.get_env(:ex_microsoft_azure_management_samples, :subscription_id)
-  end
-
-  def tenant_id() do
-     Application.get_env(:ex_microsoft_azure_management_samples, :tenant_id)
-  end
-
-  def token() do
-    %{"accessToken" => token} =
-      System.user_home!()
-      |> Path.absname()
-      |> Path.join(".azure")
-      |> Path.join("accessTokens.json")
-      |> File.read!()
-      |> Poison.decode!()
-      |> Enum.filter(&( &1["_authority"] == "https://login.microsoftonline.com/" <>  tenant_id()))
-      |> List.last()
-
-    token
-  end
-
-  def connection() do
-    token()
-    |> AzureManagementClient.new()
-  end
-
   test "list resource groups" do
-    conn = connection()
-
-    api_version = "2018-02-01"
-
-    {:ok, %{value: groups}} =
-      conn
-      |> ResourceGroups.resource_groups_list(
-        api_version,
-        subscription_id()
-      )
-
-    ids = groups |> Enum.map(&(&1 |> Map.get(:id)))
-
-    ids |> Enum.join("\n") |> IO.puts()
+    ExMicrosoftAzureManagementSamples.list_resource_groups()
+    |> Enum.join("\n")
+    |> IO.puts()
   end
 
   test "list VM sizes" do
-    conn = connection()
-    api_version = "2017-12-01"
-    location = "westeurope"
-
-    {:ok, %{value: sizes}} =
-      conn
-      |> VirtualMachineSizes.virtual_machine_sizes_list(
-        location,
-        api_version,
-        subscription_id()
-      )
-
-    names = sizes |> Enum.map(&(&1 |> Map.get(:name)))
-
-    names |> Enum.join("\n") |> IO.puts()
+    ExMicrosoftAzureManagementSamples.list_vm_sizes()
+    |> Enum.join("\n")
+    |> IO.puts()
   end
 end
